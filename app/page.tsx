dc6d667b52b7cd6cd18d91b051908e4f2cf20cd7
Jarren San Jose <sanjose.jarren@gmail.com>
2024-12-10 20:41:13 -0800

    update footer to say this is a work in progress

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <StandupInput />
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        footer
+        this is a work in progress
       </footer>
     </div>
   );
