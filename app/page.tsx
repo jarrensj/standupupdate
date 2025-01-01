e5927f21893dea46ae05fa3c62a8172883824c0c
Jarren San Jose <sanjose.jarren@gmail.com>
2025-01-01 00:27:28 -0800

    create footer component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import StandupInput from "../components/StandupInput";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -12,9 +13,7 @@ export default function Home() {
         </p>
         <StandupInput />
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        this is a work in progress
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      this is a work in progress
+    </footer>
+  );
+}
