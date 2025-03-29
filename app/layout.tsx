04060de5e42255f15706727125c1a9d25c6634e5
Jarren San Jose <sanjose.jarren@gmail.com>
2025-03-28 19:46:07 -0700

    edit z-index for user auth status / sign in button

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}
       >
-        <div className="absolute top-4 right-4">
+        <div className="absolute top-4 right-4 z-50">
           <SignedIn>
             <UserButton />
           </SignedIn>
