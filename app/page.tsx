5c0d60d23c02a7e574ef0235b157bc6c27e6111e
Jarren San Jose <sanjose.jarren@gmail.com>
2025-03-28 19:51:42 -0700

    add footer back

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,25 @@
 
 import StandupInput from "../components/StandupInput"
 import MatchaBackground from "../components/MatchaBackground"
+import Footer from "../components/Footer"
 
 export default function Home() {
   return (
-    <main className="relative min-h-screen w-full flex items-center justify-center">
+    <main className="relative min-h-screen w-full flex flex-col">
       <div className="absolute inset-0 z-0 overflow-hidden">
         <MatchaBackground />
       </div>
 
       <h1 className="absolute top-6 left-6 text-3xl font-bold z-20">gmatcha</h1>
 
-      <div className="relative z-10 w-full max-w-4xl px-4">
-        <StandupInput />
+      <div className="flex-grow flex items-center justify-center">
+        <div className="relative z-10 w-full max-w-4xl px-4">
+          <StandupInput />
+        </div>
+      </div>
+
+      <div className="relative z-20 w-full py-4">
+        <Footer />
       </div>
     </main>
   )
